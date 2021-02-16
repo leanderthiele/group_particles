cea77a800c4d1ffcbc480c3b949d85c45ed148cc
Leander Thiele <lthiele@princeton.edu>
2021-02-16 16:52:34 -0500

    icc unable to compile this

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 module load hdf5/gcc/1.10.0
 module load rh/devtoolset/8
 
-icc -std=c++17 -O3 -ffast-math -g3 -Wall -Wextra -Wno-unused-parameter -Wno-reorder -o $1 $1.cpp -lhdf5 -lhdf5_cpp
+g++ -std=c++17 -O3 -ffast-math -g3 -Wall -Wextra -Wno-unused-parameter -Wno-reorder -o $1 $1.cpp -lhdf5 -lhdf5_cpp
