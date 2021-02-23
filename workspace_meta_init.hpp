547878f47b2a5b9d16253685c16b51cc16e08907
leander <leander97@gmx.de>
2021-02-23 18:38:46 -0500

    added new meta_init and hdf5_utils files

---
+++ workspace_meta_init.hpp
@@ -0,0 +1,28 @@
+#ifndef WORKSPACE_META_INIT_HPP
+#define WORKSPACE_META_INIT_HPP
+
+#include "H5Cpp.h"
+
+#include "workspace.hpp"
+
+template<typename AFields>
+void
+Workspace<AFields>::meta_init ()
+{
+    std::string fname;
+
+    // look at the first group chunk
+    callback.grp_chunk(0, fname);
+    auto fptr_grp = std::make_shared<H5::H5File>(fname, H5F_ACC_RDONLY);
+    callback.read_grp_meta_init(fptr_grp);
+    fptr_grp->close();
+
+    // look at the first group chunk
+    callback.prt_chunk(0, fname);
+    auto fptr_prt = std::make_shared<H5::H5File>(fname, H5F_ACC_RDONLY);
+    callback.read_prt_meta_init(fptr_prt);
+    fptr_prt->close();
+}
+
+
+#endif // WORKSPACE_META_INIT_HPP
