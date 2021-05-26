0613f05fba75f5fe2aa307ec2d816408c1d14663
leander <leander97@gmx.de>
2021-05-26 00:13:35 -0400

    added compilation script for different particle types

---
+++ compile_parttype.sh
@@ -0,0 +1,9 @@
+# set to one of GAS, DM, STARS, BH
+PartType=DM
+
+g++ -std=c++17 -O3 -ffast-math -funroll-loops \
+  -D$PartType \
+  -g3 -Wall -Wextra -Wno-unused-parameter -Wno-reorder \
+  -I./include -I./include/callback_utils -I./detail \
+  -o $1_$PartType $1.cpp \
+  -lhdf5 -lhdf5_cpp -fopenmp
